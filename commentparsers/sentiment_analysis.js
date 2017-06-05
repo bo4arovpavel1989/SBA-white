1b4c422a718022ad9b352f65e5d268d4b742f624
Unknown <bo4arovpavel1989@mail.ru>
2017-06-05 09:37:22 +0300

    started sentiment_analysis

---
+++ commentparsers/sentiment_analysis.js
@@ -0,0 +1,15 @@
+var Feedback = require('./../lib/models/mongoModel.js').Feedback;
+var Complaint = require('./../lib/models/mongoModel.js').Complaint;
+var Comment = require('./../lib/models/mongoModel.js').Comment;
+var analyze = require('sentimental').analyze;
+Comment.find({}, (err, reps)=>{
+	if(reps){
+		reps.forEach(rep=>{
+			let score = analyze(rep.comment);
+			console.log('===============\n');
+			console.log('score: ' + score.score + '\n');
+			console.log(rep.comment);
+			console.log('===============\n');
+		});
+	}
+});
