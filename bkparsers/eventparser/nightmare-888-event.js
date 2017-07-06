37d547b6c65538a8f359cec68b15324511de7946
Unknown <bo4arovpavel1989@mail.ru>
2017-07-06 09:38:57 +0300

    parsed 888 event. need to fix callback

--- bkparsers/eventparser/nightmare-888-event.js
+++ bkparsers/eventparser/nightmare-888-event.js
@@ -2,16 +2,16 @@ var Nightmare = require('nightmare');
 var nightmare = Nightmare({ show: false });
 var cheerio = require('cheerio');
 var objectToReturn = {
-	bk: '1xstavka'
+	bk: '888'
 };
+console.log('888-parser');
 
-console.log('1xstavka-parser');
 
 let grabEvent = function (link, callback){
- nightmare
+nightmare
   .goto(link)
   .useragent("Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36")
-  .wait(1500)
+  .wait('.WINNER')
   .evaluate(function () {
 	return document.body.innerHTML;
   })
@@ -19,22 +19,24 @@ let grabEvent = function (link, callback){
   .then(function (body) {
      var $ = cheerio.load(body);
 	 //console.log($.html());
-	let lines=$('div.bets').get();
+	let lines = $('.WINNER').get();
 	let market1x2=lines[0];
+	//console.log(market1x2);
 	let win, draw, away, marja;
-	win = market1x2.children[8].attribs['data-coef'];
-	if (market1x2.children[30] != undefined) {
-		draw = market1x2.children[19].attribs['data-coef'];
-		away= market1x2.children[30].attribs['data-coef'];
-	} else {
-		draw = '-';
-		away = market1x2.children[19].attribs['data-coef'];
+	win=market1x2.children[0].children[0].children[1].children[0].children[0].data;
+	//console.log(win);
+	if(market1x2.children.length == 3) {
+		draw=market1x2.children[1].children[0].children[1].children[0].children[0].data;
+		away=market1x2.children[2].children[0].children[1].children[0].children[0].data;
+	}else{
+		draw='-'
+		away=market1x2.children[1].children[0].children[1].children[0].children[0].data;
 	}
 	marja = 0;
-				if(win != '-' && win != 0) marja += 100/parseFloat(win);
-				if(draw != '-' && draw != 0) marja += 100/parseFloat(draw);
-				if(away != '-' && away != 0) marja += 100/parseFloat(away);
-				marja = marja -100;
+	if(win != '-' && win != 0) marja += 100/parseFloat(win);
+	if(draw != '-' && draw != 0) marja += 100/parseFloat(draw);
+	if(away != '-' && away != 0) marja += 100/parseFloat(away);
+	marja = marja -100;
 	console.log(win + ' - ' + draw + ' - ' + away + ". Marja: " + marja);	
 	objectToReturn.win = win;
 	objectToReturn.draw = draw;
@@ -45,23 +47,23 @@ let grabEvent = function (link, callback){
   .catch(function (error) {
 	console.log(error);
 	callback(error, null);
-  });	
-}
+  });
+};
 
-grabEvent('https://1xstavka.ru/line/Tennis/69371-Wimbledon/13601085-Grigor-Dimitrov-Marcos-Baghdatis/');
+grabEvent('http://mobile.888.ru/#/match/7204351?type=1');
+
 module.exports.grabEvent = grabEvent;
 
 setTimeout(()=>{
 	console.log('timeout stop');
 	if(nightmare) {
 		try{
-			nightmare.end();
-			nightmare.proc.disconnect();
-			nightmare.proc.kill();
-			nightmare.ended = true;
-			nightmare = null;
+		nightmare.end();
+		nightmare.proc.disconnect();
+		nightmare.proc.kill();
+		nightmare.ended = true;
+		nightmare = null;
 		}catch(e){}
 	}
 }, 5*60*1000);
-
   
