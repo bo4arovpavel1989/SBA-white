805e84bac22fa7cc55358f89e0273971d692a218
Unknown <bo4arovpavel1989@mail.ru>
2017-08-28 09:57:39 +0300

    continued reputation page
    made calculatedynamics script

--- public/scripts/online_reputationpage.js
+++ public/scripts/online_reputationpage.js
@@ -20,6 +20,7 @@ function showReputationDataHandler(){
 						document.getElementById('showData').disabled = false;
 						console.log(data);
 						$('#reputationDataContainer').empty();
+						$('#lastCommentsContainer').empty();
 						calculateDynamics(data);
 				}
 		});
@@ -31,5 +32,43 @@ function calculateDynamics(data){
 	console.log(commentObject);
     var templateComment = Handlebars.compile( $('#lastComments').html() );
 	var commentHTML=templateComment(commentObject);
-	$('#reputationDataContainer').append( commentHTML );
+	$('#lastCommentsContainer').append( commentHTML );
+	var nowFeedback=data.to;
+	var firstFeedback=data.from;
+	var sourcesNow=[];
+	var sourcesFirst=[];
+	var nowStats={};
+	var firstStats={};
+	var dynamicsStat={};
+	nowFeedback.forEach(function(feedback){
+		if(sourcesNow.indexOf(feedback.source)===-1) {
+			sourcesNow.push(feedback.source);
+			nowStats[feedback.source]={isPositive:0,isNegative:0,isNeutral:0}
+		}
+	});
+	firstFeedback.forEach(function(feedback){
+		if(sourcesFirst.indexOf(feedback.source)===-1) {
+			sourcesFirst.push(feedback.source);
+			firstStats[feedback.source]={isPositive:0,isNegative:0,isNeutral:0}
+		}
+	});
+	nowFeedback.forEach(function(feedback){
+		if(feedback.isPositive)nowStats[feedback.source].isPositive++; 
+		if(feedback.isNegative)nowStats[feedback.source].isNegative++; 
+		if(feedback.isNeutral)nowStats[feedback.source].isNeutral++; 
+	});
+	firstFeedback.forEach(function(feedback){
+		if(feedback.isPositive)firstStats[feedback.source].isPositive++; 
+		if(feedback.isNegative)firstStats[feedback.source].isNegative++; 
+		if(feedback.isNeutral)firstStats[feedback.source].isNeutral++; 
+	});
+	for(var property in nowStats){
+		dynamicsStat[property]={};
+		firstStats[property] = firstStats[property] ?  firstStats[property] : {isPositive:0,isNegative:0,isNeutral:0};
+		dynamicsStat[property].isPositive=nowStats[property].isPositive - firstStats[property].isPositive;
+		dynamicsStat[property].isNegative=nowStats[property].isNegative - firstStats[property].isNegative;
+		dynamicsStat[property].isNeutral=nowStats[property].isNeutral - firstStats[property].isNeutral;
+	}
+	console.log(dynamicsStat);
+	console.log(nowStats);
 }
