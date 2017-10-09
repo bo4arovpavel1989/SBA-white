2d40e554e14236d7172e5139d9e9f3c05d091989
Unknown <bo4arovpavel1989@mail.ru>
2017-10-09 15:46:18 +0300

    made event writing in DB

--- bkparsers/parserController-events.js
+++ bkparsers/parserController-events.js
@@ -8,71 +8,83 @@ var grabBkolimp=require('./eventparser/nightmare-bkolimp-event.js').grabEvent;
 var grabBaltbet=require('./eventparser/nightmare-baltbet-event.js').grabEvent;
 var grabBetcity=require('./eventparser/nightmare-betcity-event.js').grabEvent;
 var EventToGrab = require('./../lib/models/mongoModel.js').EventToGrab;
+var EventGrabbed = require('./../lib/models/mongoModel.js').EventGrabbed;
 var async=require('async');
 
 function parseEvents(objectToGet){
-	var objectToWrite={};
+	var objectToWrite={bk:{}};
 	console.log(objectToGet);
 	async.parallel([
 	(callback)=>{
-		grabLeon(objectToGet.leon, (err, rep)=>{
-			objectToWrite.leon=rep;
+		if (objectToGet.leon)grabLeon(objectToGet.leon, (err, rep)=>{
+			objectToWrite.bk.leon=rep;
 			callback();
 		});
+		else callback();
 	},
 	(callback)=>{
-		grabFonbet(objectToGet.fonbet, objectToGet.fonbetType, (err, rep)=>{
-			objectToWrite.fonbet=rep;
+		if(objectToGet.fonbet)grabFonbet(objectToGet.fonbet, objectToGet.fonbetType, (err, rep)=>{
+			objectToWrite.bk.fonbet=rep;
 			callback();
 		});
+		else callback();
 	},
 	(callback)=>{
-		grabBk888(objectToGet.bk888, (err, rep)=>{
-			objectToWrite.bk888=rep;
+		if(objectToGet.bk888)grabBk888(objectToGet.bk888, (err, rep)=>{
+			objectToWrite.bk.bk888=rep;
 			callback();
 		});
+		else callback();
 	},
 	(callback)=>{
-		grabBk1xbet(objectToGet.bk1xbet, (err, rep)=>{
-			objectToWrite.bk1xbet=rep;
+		if(objectToGet.bk1xbet)grabBk1xbet(objectToGet.bk1xbet, (err, rep)=>{
+			objectToWrite.bk.bk1xbet=rep;
 			callback();
 		});
+		else callback();
 	},
 	(callback)=>{
-		grabLigastavok(objectToGet.ligastavok, (err, rep)=>{
-			objectToWrite.ligastavok=rep;
+		if(objectToGet.ligastavok)grabLigastavok(objectToGet.ligastavok, (err, rep)=>{
+			objectToWrite.bk.ligastavok=rep;
 			callback();
 		});
+		else callback();
 	},
 	(callback)=>{
-		grabWinline(objectToGet.winline, (err, rep)=>{
-			objectToWrite.winline=rep;
+		if(objectToGet.winline)grabWinline(objectToGet.winline, (err, rep)=>{
+			objectToWrite.bk.winline=rep;
 			callback();
 		});
+		else callback();
 	},(callback)=>{
-		grabBkolimp(objectToGet.bkolimp, (err, rep)=>{
-			objectToWrite.bkolimp=rep;
+		if(objectToGet.bkolimp)grabBkolimp(objectToGet.bkolimp, (err, rep)=>{
+			objectToWrite.bk.bkolimp=rep;
 			callback();
 		});
+		else callback();
 	},
 	(callback)=>{
-		grabBaltbet(objectToGet.baltbet, (err, rep)=>{
-			objectToWrite.baltbet=rep;
+		if(objectToGet.baltbet)grabBaltbet(objectToGet.baltbet, (err, rep)=>{
+			objectToWrite.bk.baltbet=rep;
 			callback();
 		});
+		else callback();
 	},
 	(callback)=>{
-		grabBetcity(objectToGet.betcity, (err, rep)=>{
-			objectToWrite.betcity=rep;
+		if(objectToGet.betcity)grabBetcity(objectToGet.betcity, (err, rep)=>{
+			objectToWrite.bk.betcity=rep;
 			callback();
 		});
+		else callback();
 	}],
 		(err)=>{
 			let time = getTime();
 			console.log(time+'/r/n');
 			console.log(objectToWrite);
-			objectToWrite=JSON.stringify(objectToWrite);
-			redisClient.lpush('eventCoefficients', objectToWrite);
+			objectToWrite.time=time;
+			for (var prop in objectToWrite.bk){
+				EventGrabbed.update({name:objectToGet.name,bk:prop},{$push:{data:{time: objectToWrite.time,data:objectToWrite.bk[prop]}}},{upsert:true}).exec();
+			}
 		}
 	);
 }
@@ -107,7 +119,7 @@ function startEventGrabbing(){
 			setInterval(()=>{
 				parseEvents(eventObject);
 			},
-			5*60*1000)
+			3*60*1000)
 		});
 	}
 
