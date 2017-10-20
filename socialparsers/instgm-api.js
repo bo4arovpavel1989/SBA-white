fcba14475019f082bd3af4e93a945db1eb7fac2b
Unknown <bo4arovpavel1989@mail.ru>
2017-10-20 09:45:46 +0300

    added admin forms for social and line pages

--- socialparsers/instgm-api.js
+++ socialparsers/instgm-api.js
@@ -38,6 +38,8 @@ function getINSTAData(i){
 		try{
 			obj.posts=$('#react-root > section > main > article > header > div._o6mpc > ul > li:nth-child(1) > span > span').get()[0].children[0].data;
 			obj.sbcrb=$('#react-root > section > main > article > header > div._o6mpc > ul > li:nth-child(2) > span > span').get()[0].attribs.title;
+			obj.sbcrb=obj.sbcrb.replace(/\s/ig, '');
+			obj.sbcrb=Number(obj.sbcrb);
 			obj.link='https://www.instagram.com/'+bks[i];
 		}catch(e){obj.posts='N/A';obj.sbcrb='N/A'}
 		obj.comments='N/A';
