b1bc79b94130ad4b0d68779abd5524a2cd9d5689
Unknown <bo4arovpavel1989@mail.ru>
2017-10-24 09:46:41 +0300

    added other bk to hh_api
    need to check marathon and make older vacancies attouch to new added BK names

--- hh_api/hh_api_first.js
+++ hh_api/hh_api_first.js
@@ -1,14 +1,14 @@
 var https=require('https');
 var Vacancy = require('./../lib/models/mongoModel.js').Vacancy;
-var bks = ['-', '1066298', '2599435', '-', '991318', '-', '223129', '91449', '782901'];
-var bks_formatted=require('./../bklist.js').bkList;//bk names as i use it in ither modules
-
-
+var bks = ['leon', 1066298, 1206335, 2757107, 991318, 1860203, 223129, 91449, 782901,'atlantik-m',1795968,'betru',1968591, 
+'favorit',3051080,'investcompcentr', 'investgarant','johnygame',53796,'matchbet',"melofon",'panorama','rosbet','rosippodrom','rusteletot','Sportbet',
+2707907,'williamhill', 'aphrodita','evromir', 'metelitsa', 'mlb','pyatigorskiippodrom', 'rostovskiippodrom'];
+var bks_formatted=require('./../bklist.js').bkList_offline;//bk names as i use it in ither modules
 
 getVacancies(0);
 
 function getVacancies(i){
-				if(bks[i]!='-'){
+				if(!isNaN(bks[i])){ //coz company id in hh is number
 				var options = {
 					host: 'api.hh.ru',
 					port: 443,
@@ -30,32 +30,33 @@ function getVacancies(i){
 						let quantity = data.found;
 						let pages = data.pages;
 						let vacancies = data.items;
-						vacancies.forEach(vacancy=>{
-							try {
-							let salary 
-							if(vacancy.salary) {
-								vacancy.salary.from=(vacancy.salary.from===null) ? 'n/a' : vacancy.salary.from;
-								vacancy.salary.to=(vacancy.salary.to===null) ? 'n/a' : vacancy.salary.to;
-								salary = vacancy.salary.from + ' - ' + vacancy.salary.to + ' ' + vacancy.salary.currency;
-							} else salary = '-';
-							let objectToWrite={
-									bk:bks_formatted[i].bk,
-									company: vacancy.employer.name,
-									type: vacancy.name,
-									area: vacancy.area.name,
-									salary: salary,
-									link: vacancy.url,
-									description: vacancy.snippet.responsibility,
-									created_at:vacancy.created_at
-								};
-								console.log(objectToWrite);
-							let vacancyToWrite=new Vacancy(objectToWrite).save(()=>{
-								i++;
-								if(i<bks.length)getVacancies(i);
-								else console.log('done');
-							});
-							} catch(e){}
-						});
+						if(vacancies) vacancies.forEach(vacancy=>{
+										try {
+										let salary 
+										if(vacancy.salary) {
+											vacancy.salary.from=(vacancy.salary.from===null) ? 'n/a' : vacancy.salary.from;
+											vacancy.salary.to=(vacancy.salary.to===null) ? 'n/a' : vacancy.salary.to;
+											salary = vacancy.salary.from + ' - ' + vacancy.salary.to + ' ' + vacancy.salary.currency;
+										} else salary = '-';
+										let objectToWrite={
+												bk:bks_formatted[i].bk,
+												company: vacancy.employer.name,
+												type: vacancy.name,
+												area: vacancy.area.name,
+												salary: salary,
+												link: vacancy.url,
+												description: vacancy.snippet.responsibility,
+												created_at:vacancy.created_at
+											};
+											console.log(objectToWrite);
+										let vacancyToWrite=new Vacancy(objectToWrite).save((err, rep)=>{
+											i++;
+											if(i<bks.length)getVacancies(i);
+											else console.log('done');
+										});
+										} catch(e){}
+									});
+						else {console.log(bks[i]);}			
 						if(quantity>100&&page<pages) getVacancies(page +1);
 					
 						
