c1e6a2f7761cb86d27b66c973501af30681fb6c9
Unknown <bo4arovpavel1989@mail.ru>
2017-10-26 16:02:17 +0300

    need to check bkPopluarity.js

--- ppsInfo/writePPSfromFNS.js
+++ ppsInfo/writePPSfromFNS.js
@@ -23,26 +23,26 @@ module.exports.writingPPSfromFNS=function(k, callback){
 		var path="ppsAddressesFromFNSXML/"+bk.bk+'/'+filename;
 		console.log(path);
 		fs.readFile(path, 'utf8',(err, rep)=>{
-					var j=0;
-					rep = rep.split('\n');
-					var control2=rep.length;		
-					let nameSplit=filename.split('_');
-					let dateBegin=nameSplit[1];
-					let dateEnd=nameSplit[2];
-					if(dateEnd=='.dat'||dateEnd=='.DAT'){
-						var today = new Date();
-						dateEnd=today.getFullYear()+'-'+(today.getMonth()+2)+'-01';
-					} 
-					console.log(dateBegin);
-					console.log(dateEnd);
-					console.log(filename);
-					rep.forEach(line=>{
-							var bkPPS = new BkPPS({bk:bk.bk, name:bk.name, begin:dateBegin, end:dateEnd, address: line}).save((err, reply)=>{
-								j++;
-								if(j==control2) callbackJunior(true);
-							});			   
-					});				
-				});	
+			var j=0;
+			rep = rep.split('\n');
+			var control2=rep.length;		
+			let nameSplit=filename.split('_');
+			let dateBegin=nameSplit[1];
+			let dateEnd=nameSplit[2];
+			if(dateEnd=='.dat'||dateEnd=='.DAT'){
+				var today = new Date();
+				dateEnd=today.getFullYear()+'-'+(today.getMonth()+2)+'-01';
+			} 
+			console.log(dateBegin);
+			console.log(dateEnd);
+			console.log(filename);
+			rep.forEach(line=>{
+					var bkPPS = new BkPPS({bk:bk.bk, name:bk.name, begin:dateBegin, end:dateEnd, address: line}).save((err, reply)=>{
+						j++;
+						if(j==control2) callbackJunior(true);
+					});			   
+			});				
+		});	
 	}
 	
 };
