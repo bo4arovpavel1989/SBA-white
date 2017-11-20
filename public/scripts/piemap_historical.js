2e176b7c9e5f0d674a37b9633fd6e56bec1d5ee5
bo4arovpavel1989 <bo4arovpavel1989@mail.ru>
2017-11-20 19:46:37 +0300

    continued dynamic map
    need to clear map and make showing date

--- public/scripts/piemap_historical.js
+++ public/scripts/piemap_historical.js
@@ -1,5 +1,6 @@
 var filter=[];
 var points=[];
+var myMap, myPlacemark;
 $(document).ready(function(){
 	var checkMapLoading = setInterval(function(){ checkingMap() }, 1000);
 
@@ -21,6 +22,14 @@ $(document).ready(function(){
 function startAll(){
 	$('.loader').removeClass('loading');
 	$('#formToHide').show(400);
+	ymaps.ready(init);
+
+    function init(){     
+        myMap = new ymaps.Map("map", {
+            center: [55.76, 37.64],
+            zoom: 4
+        });
+	}	
 	getHistoricalData();
 }
 
@@ -38,12 +47,14 @@ function getHistoricalData(){
 				processData: false,
 				data: formData,
 				success: function(data){
-						$('.loader').removeClass('loading');
-						drawHistoricalMap(data);
+					$('.loader').removeClass('loading');
+					document.getElementById('showData').disabled = false;
+					drawHistoricalMap(data);
 				},
 				error:function(e){
 					$('.loader').removeClass('loading');
 					if(e)alert('Ошибка!');
+					document.getElementById('showData').disabled = false;
 				}
 		});
 	});
@@ -51,4 +62,38 @@ function getHistoricalData(){
 
 function drawHistoricalMap(data){
 	console.log(data);
+	getDataForDate(data.dates,0,data.bk);
+}
+
+function getDataForDate(date,i,bk){
+	var year=date[i].split('-')[0];
+	var month=date[i].split('-')[1];
+	points=[];
+	$.ajax({
+		url: '/bkpps_historical/'+year+'/'+month+'/ppscoordinates'+bk+'.json',
+		dataType: 'json',
+		success: function(data){
+			console.log(data);
+			data.forEach(function(dat){
+				points.push({bk: dat.bk, coords:dat.data.geometry.coordinates, description: dat.data.properties.description});
+			});
+			drawMapForDate(date[i],function(){
+				i++;
+				if(i<date.length)getDataForDate(date,i,bk);
+			});
+		}		
+	});
+}
+
+function drawMapForDate(date,callback){
+	alert(date);
+	console.log(points);
+	var filterView = document.getElementById("showData");
+	filterView.scrollIntoView({block: "start", behavior: "smooth"});
+	points.forEach(function(point){
+		console.log(point.coords);
+        myPlacemark = new ymaps.Placemark(point.coords);
+        myMap.geoObjects.add(myPlacemark);
+	});
+	setTimeout(function(){callback();},5000);
 }
