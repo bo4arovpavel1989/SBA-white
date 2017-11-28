478ab92687438f7040f50d4e429c24b54dfdb0ae
Unknown <bo4arovpavel1989@mail.ru>
2017-11-28 09:49:18 +0300

    delete only neccessary when TOOMANYREQ occured

--- ppsInfo/getHistoricalPPSCoordinates.js
+++ ppsInfo/getHistoricalPPSCoordinates.js
@@ -19,7 +19,7 @@ provider.getText = function (point) {
 
 
 var actualDate=new Date();
-var date1='2010-01-01';//start from here coz i got coords of previous time
+var date1='2012-10-15';//start from here coz i got coords of previous time
 var date2=actualDate.getFullYear()+'-'+actualDate.getMonth()+'-15'; //date of previous month
 if(actualDate.getMonth()<10) date2=actualDate.getFullYear()+'-0'+actualDate.getMonth()+'-15'; 
 if(actualDate.getMonth()==0)  date2=actualDate.getFullYear()-1+'12-15';
@@ -100,7 +100,7 @@ function getCoordinates(j,date,callback){
 						setTimeout(()=>{timeout=false},4*60*60*1000) //wait 4 hours coz its too many requests
 						console.log('TOO MANY REQUESTS timeout 4 hours');
 						while(timeout){	}
-						BkPPSCoordinates.remove({month:month,year:year}).exec();
+						BkPPSCoordinates.remove({bk:shortname,month:month,year:year}).exec();
 						recursion(j,date);
 					}
 				});
