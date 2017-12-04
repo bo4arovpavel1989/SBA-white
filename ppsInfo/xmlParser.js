b4ce8c1b7427145e1670bc281d8f98517de00362
bo4arovpavel1989 <bo4arovpavel1989@mail.ru>
2017-12-04 19:27:04 +0300

    refreshed data from fns

--- ppsInfo/xmlParser.js
+++ ppsInfo/xmlParser.js
@@ -30,7 +30,7 @@ let headers = {
 	};
 
 let options = {
-			url:'https://www.nalog.ru/opendata/7707329152-reestrlicbookmaker/data-20170929-structure-20160803.xml',
+			url:'https://www.nalog.ru/opendata/7707329152-reestrlicbookmaker/data-30171120-structure-30160803.xml',
 			headers:headers,
 			encoding:null	
 	}; 
